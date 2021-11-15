68caf9800ed1d0ad77390e86ee79f6447d16f04b
Nicholas <nicholas@usp.br>
2021-11-15 17:22:43 -0300

    Adcionando arquivos de Conversões e variáveis

---
+++ Conversions/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1000;
+            byte b = (byte)i;
+
+            var number = "1234";
+            int z = Convert.ToInt32(number);
+
+            try
+            {
+                var numberTwo = "1234";
+                byte bTwo = Convert.ToByte(numberTwo);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro de conversão");
+            }
+
+
+            Console.WriteLine(b);
+        }
+    }
+}
