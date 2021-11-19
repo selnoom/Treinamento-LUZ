0383a7bbe2e4d9ef4579dfc18ca1fe465a55ee34
Nicholas <nicholas@usp.br>
2021-11-18 23:22:00 -0300

    Reogranizando pastas

--- WPF/CRUD 1.0/MainWindowsViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace CRUD_1._0
-{
-    public class MainWindowsViewModelBase
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-    }
-}
